9e973cc9db5ea27e5d7d2fb7cae6fe548f580a0f
Sam Knight <sam@samknight.co.uk>
2016-08-30 21:53:03 +0100

    Show beginning of day by default

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,10 +2,20 @@ class VideosController < ApplicationController
 
   def show
     day = Debate.find_by(tv: params[:id]).log_date
-    @speech = Speech.find_by(log_date: day, spoke_at: DateTime.parse(params[:timestamp]).in_time_zone)
+    @speech = find_speech(day, params[:timestamp])
   end
+
+  private
 
   def video_params
     params.permit(:id, :timestamp, :autostart, :captions)
+  end
+
+  def find_speech(day, timestamp = nil)
+    if timestamp
+      Speech.find_by(log_date: day, spoke_at: DateTime.parse(timestamp).in_time_zone)
+    else
+      Speech.find_by(log_date: day)
+    end
   end
 end
